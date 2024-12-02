8f6243b3b2737c5fc90fee84f7f79d5ccd0e168e
Parth Vaidya <zeusparth99@gmail.com>
2024-12-02 19:48:45 +0530

    BodyPart added

---
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "UI/UIElement/ImageView.h"
+
+class BodyPart
+{
+protected:
+	UI::UIElement::ImageView* bodypart_image;
+
+	sf::Vector2i grid_position;
+	Direction direction;
+
+	float bodypart_width;
+	float bodypart_height;
+}
