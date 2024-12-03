c5f8853ec1eae72e511bad5dc4fc2b32ae797b0b
Parth Vaidya <zeusparth99@gmail.com>
2024-12-03 15:01:38 +0530

    LinkedList created

---
+++ Linked-List-Snake/include/LinkedList/SingleLinkedList.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SFML/System/Vector2.hpp>
+#include "Player/Node.h"
+#include "Player/Direction.h"
+
+namespace LinkedList
+{
+	class SingleLinkedList
+	{
+	private:
+		Node* head_node;
+
+		float node_width;
+		float node_height;
+
+		sf::Vector2i default_position;
+		Direction default_direction;
+
+		Node* createNode();
+
+	public:
+		SingleLinkedList();
+		~SingleLinkedList();
+
+		void initialize(float width, float height, sf::Vector2i position, Direction direction);
+		void render();
+	};
+}
