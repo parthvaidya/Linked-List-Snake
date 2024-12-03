395fff5091dcf895f28eef2a5f3cfd4a314a2964
Parth Vaidya <zeusparth99@gmail.com>
2024-12-03 11:19:49 +0530

    CALCULATING SCREEN POSITION

--- Linked-List-Snake/source/Player/BodyPart.cpp
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -32,4 +32,39 @@ namespace Player
 		initializeBodyPartImage();
 	}
 
+	sf::Vector2f BodyPart::getBodyPartScreenPosition()
+	{
+		float x_screen_position = LevelView::border_offset_left + (grid_position.x * bodypart_width) + (bodypart_width / 2);
+		float y_screen_position = LevelView::border_offset_top + (grid_position.y * bodypart_height) + (bodypart_height / 2);
+
+		return sf::Vector2f(x_screen_position, y_screen_position);
+	}
+
+
+	float BodyPart::getRotationAngle()
+	{
+		switch (direction)
+		{
+		case Direction::UP:
+			return 270.f;
+		case Direction::DOWN:
+			return 90.f;
+		case Direction::RIGHT:
+			return 0;
+		case Direction::LEFT:
+			return 180.f;
+		}
+	}
+
+	void BodyPart::setDirection(Direction direction)
+	{
+		this->direction = direction;
+	}
+
+	void BodyPart::updatePosition()
+	{
+		bodypart_image->setPosition(getBodyPartScreenPosition());
+		bodypart_image->setRotation(getRotationAngle());
+		bodypart_image->update();
+	}
 }
