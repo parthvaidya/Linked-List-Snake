626bbf796e2fcc4ba936f3d3345e229d775e7a28
Parth Vaidya <zeusparth99@gmail.com>
2024-12-03 15:03:51 +0530

    Spawn Head

--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -8,9 +8,7 @@ namespace LinkedList
 	{
 		head_node = nullptr;
 	}
-
 	SingleLinkedList::~SingleLinkedList() = default;
-
 	void SingleLinkedList::initialize(float width, float height, sf::Vector2i position, Direction direction)
 	{
 		node_width = width;
@@ -18,11 +16,19 @@ namespace LinkedList
 		default_position = position;
 		default_direction = direction;
 	}
-
-	void SingleLinkedList::render() { }
-
+	void SingleLinkedList::render()
+	{
+		head_node->body_part.render();
+	}
+	void SingleLinkedList::createHeadNode()
+	{
+		head_node = createNode();
+		head_node->body_part.initialize(node_width, node_height, default_position, default_direction);
+		return;
+	}
 	Node* SingleLinkedList::createNode()
 	{
 		return new Node();
 	}
+}}
 }
