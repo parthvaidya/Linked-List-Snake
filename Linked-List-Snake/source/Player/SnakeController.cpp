62dc531979407cc532ce86d49513df9dc9980cb8
Parth Vaidya <zeusparth99@gmail.com>
2024-12-04 09:28:05 +0530

    Reset respawn and restart

--- Linked-List-Snake/source/Player/SnakeController.cpp
+++ Linked-List-Snake/source/Player/SnakeController.cpp
@@ -126,6 +126,32 @@ namespace Player
 		}
 	}
 
+	void SnakeController::reset()
+	{
+		current_snake_state = SnakeState::ALIVE;
+		current_snake_direction = default_direction;
+		elapsed_duration = 0.f;
+		restart_counter = 0.f;
+	}
+
+	void SnakeController::respawnSnake()
+	{
+		single_linked_list->removeAllNodes();
+		reset();
+		spawnSnake();
+	}
+
+	void SnakeController::handleRestart()
+	{
+		restart_counter += ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
+
+		if (restart_counter >= restart_duration)
+		{
+			respawnSnake();
+		}
+	}
+
+
 	void SnakeController::delayedUpdate()
 	{
 		elapsed_duration += ServiceLocator::getInstance()->getTimeService()->getDeltaTime();
