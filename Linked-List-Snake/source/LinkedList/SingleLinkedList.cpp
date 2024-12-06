644217bcaaea2cca45b32c5e5c5006b7218fe894
Parth Vaidya <zeusparth99@gmail.com>
2024-12-06 11:46:42 +0530

    Deleted at the end

--- Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedList/SingleLinkedList.cpp
@@ -151,6 +151,28 @@ namespace LinkedList
 		delete (cur_node);
 	}
 
+	void SingleLinkedList::removeNodeAtTail()
+	{
+		if (head_node == nullptr) return;
+		linked_list_size--; //Decrement linked list size when you are deleting a node
+
+		Node* cur_node = head_node;
+
+		if (cur_node->next == nullptr) //If there is only 1 node in the linked list
+		{
+			removeNodeAtHead();
+			return;
+		}
+
+		while (cur_node->next->next != nullptr) //If there is more than 1 node in the linked list
+		{
+			cur_node = cur_node->next;
+		}
+
+		delete (cur_node->next);
+		cur_node->next = nullptr; //Set the new tail node's next pointer to nullptr
+	}
+
 	void SingleLinkedList::insertNodeAtTail() {
 		Node* new_node = createNode();
 		Node* cur_node = head_node;
