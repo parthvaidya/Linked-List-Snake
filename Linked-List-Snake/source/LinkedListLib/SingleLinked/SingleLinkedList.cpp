3597924f69ca4b0bb70897dea075e2bc15c325e6
Parth Vaidya <zeusparth99@gmail.com>
2024-12-08 19:18:35 +0530

    INSERT node AT THe end

---
+++ Linked-List-Snake/source/LinkedListLib/SingleLinked/SingleLinkedList.cpp
@@ -0,0 +1,41 @@
+#include "LinkedListLib/SingleLinked/SingleLinkedList.h"
+#include "LinkedListLib/SingleLinked/SingleNode.h"
+#include "Level/LevelView.h"
+
+namespace LinkedListLib
+{
+    namespace SingleLinked
+    {
+        Node* SingleLinkedList::createNode()
+        {
+            return new SingleNode();
+        }
+
+        SingleLinkedList::SingleLinkedList() = default;
+
+        SingleLinkedList::~SingleLinkedList() = default;
+
+        void SingleLinkedList::insertNodeAtTail()
+        {
+            linked_list_size++;
+            Node* new_node = createNode();
+            Node* cur_node = head_node;
+
+            if (cur_node == nullptr)
+            {
+                head_node = new_node;
+                initializeNode(new_node, nullptr, Operation::TAIL);
+                return;
+            }
+
+            while (cur_node->next != nullptr)
+            {
+                cur_node = cur_node->next;
+            }
+
+            cur_node->next = new_node;
+            initializeNode(new_node, cur_node, Operation::TAIL);
+        }
+
+    }
+}
